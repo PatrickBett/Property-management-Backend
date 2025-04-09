76b5e7a53237709d7836964a7cab09771c5e0cff
Patrick <patrickbett018@gmail.com>
2025-04-09 16:45:31 +0300

    settings

--- backend/settings.py
+++ backend/settings.py
@@ -6,7 +6,7 @@ from django.conf import settings
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv()  # Load .env variables
+load_dotenv()  # Load .env variabless
 
 
 
