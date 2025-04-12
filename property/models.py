73ea9272de231d3898ae1d24e3b3c45442254369
Patrick <patrickbett018@gmail.com>
2025-04-12 15:15:52 +0300

    deploy yml updates

--- property/models.py
+++ property/models.py
@@ -12,6 +12,8 @@ class CustomUser(AbstractUser):
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
 
+    
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
